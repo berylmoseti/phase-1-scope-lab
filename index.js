c0eb7cbf8cc896e2ead7518af6317c5c3d80f895
beryl-hub-ai <nyambekimoseti@gmail.com>
2025-06-24 23:15:28 +0300

    Take me with you

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'Karen';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Chad';
+}
+
